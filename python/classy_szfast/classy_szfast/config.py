959c12849575e57bccd2f849fbf3dc928ce949f2
jyothischandran1597 <57825159+jyothischandran1597@users.noreply.github.com>
2023-07-02 22:56:36 +0200

    Update config.py

--- python/classy_szfast/classy_szfast/config.py
+++ python/classy_szfast/classy_szfast/config.py
@@ -18,10 +18,11 @@ import os
 root_path = os.path.abspath("")
 # assuming cosmopower organization codes are one level up:
 # path_to_cosmopower_organization = root_path + '/../cosmopower-organization/'
-path_to_cosmopower_organization = '/Users/boris/Work/CLASS-SZ/SO-SZ/cosmopower-organization/'
+# path_to_cosmopower_organization = '/Users/boris/Work/CLASS-SZ/SO-SZ/cosmopower-organization/'
+path_to_cosmopower_organization = '/home/jyothis/jyothis/software/class_sz/notebooks/cosmopower-organization/'
 # print(path_to_cosmopower_organization)
 
-# path_to_emulators = path_to_cosmopower_organization + 'lcdm/'
+path_to_emulators = path_to_cosmopower_organization + 'lcdm/'
 #
 # # print(path_to_emulators)
 #
